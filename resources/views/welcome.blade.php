aaa3a000a6e33ca1685b68b4e38d7a4d4ed8195f
Manuel Muñoz <manuel.munoz00@gmail.com>
2018-02-06 22:25:33 -0300

    fix title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="<?= asset('css/divinorocio.css') ?>" rel="stylesheet">
-    <title>Hello, world!</title>
+    <title>Divino Rocio</title>
   </head>
   <body>
     <!-- Begin page content -->
